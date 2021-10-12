323212db410e684dd0a393751f73e33264ee475c
Aaron <aaronsanchez01@tamu.edu>
2021-10-12 16:29:25 -0500

    Created the helper functions based off of the code from the walkthrough.

--- P3/P3/page_table.C
+++ P3/P3/page_table.C
@@ -25,8 +25,6 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
 
 PageTable::PageTable()
 {
-   assert(false);
-
    // maybe something before these things? idk it says create the first page table before
    // paging has been enabled
 
@@ -39,19 +37,20 @@ PageTable::PageTable()
    // we want to create the array here, which i think is page directory
    unsigned long address=0; // holds the physical address of where a page is
    unsigned int i;
+
    // map the first 4MB of memory
-   for(i=0; i< ENTRIES_PER_PAGE; i++)
+   for (i = 0; i < ENTRIES_PER_PAGE; i++) 
    {
       page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
       address = address + PAGE_SIZE; // 4096 = 4kb
    }
-
+   
    // fill the first entry of the page directory
    page_directory[0] = (unsigned long) page_table; // attribute set to: supervisor level, read/write, present(011 in binary)
    page_directory[0] = page_directory[0] | 3;
    // we want to then initialize the first 4MB as valid
 
-   for(i=1; i< ENTRIES_PER_PAGE; i++)
+   for(i=1; i < ENTRIES_PER_PAGE; i++)
    {
       page_directory[i] = 0 | 2; // attribute set to: supervisor level, read/write, not present(010 in binary)
    }
@@ -62,13 +61,17 @@ PageTable::PageTable()
 
 void PageTable::load()
 {
-   assert(false);
+   write_cr3(*page_directory);
+
+   current_page_table = this;
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
+   //set static enabled
+   paging_enabled = true;
+   write_cr0(*page_directory | 0x80000000);
    Console::puts("Enabled paging\n");
 }
 
