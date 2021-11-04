e3ea64139a455f778ca28f832e33acd0be34cec5
aaronsanchez01 <11447+aaronsanchez01@users.noreply.github.tamu.edu>
2021-11-04 12:40:27 -0500

    adding in p4 stuff before I work on p4 c.

---
+++ P4/P4/vm_pool.C
@@ -0,0 +1,89 @@
+/*
+ File: vm_pool.C
+ 
+ Author:
+ Date  :
+ 
+ */
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "vm_pool.H"
+#include "console.H"
+#include "utils.H"
+#include "assert.H"
+#include "simple_keyboard.H"
+#include "page_table.H"
+
+/*--------------------------------------------------------------------------*/
+/* DATA STRUCTURES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* CONSTANTS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FORWARDS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* METHODS FOR CLASS   V M P o o l */
+/*--------------------------------------------------------------------------*/
+
+VMPool::VMPool(unsigned long  _base_address,
+               unsigned long  _size,
+               ContFramePool *_frame_pool,
+               PageTable     *_page_table) {
+    // replace the assertion with your constructor code
+
+    // You need to register this VMPool with the page table.
+    // You can do that by invoking method PageTable::register_pool
+    // The page_table object file that we provide (named page_table_provided.o)
+    // contains this method.
+
+    assert(false);
+    Console::puts("Constructed VMPool object.\n");
+}
+
+unsigned long VMPool::allocate(unsigned long _size) {
+    // replace the assertion with your code
+    assert(false);
+    Console::puts("Allocated region of memory.\n");
+}
+
+void VMPool::release(unsigned long _start_address) {
+    // Notice that the regions being released may be in
+    // the page tables. We want to unmap all pages in the
+    // region being released, remove them from the page table.
+    // To unmap virtual page addr you can use
+    // method PageTable::free_page:
+    //      page_table->free_page(addr)
+    // The page_table object file that we provide (named page_table_provided.o)
+    // contains this method.
+
+        // replace the assertion with your code
+    assert(false);
+    Console::puts("Released region of memory.\n");
+}
+
+bool VMPool::is_legitimate(unsigned long _address) {
+    // replace the assertion with your code
+    assert(false);
+    return true;
+}
+
