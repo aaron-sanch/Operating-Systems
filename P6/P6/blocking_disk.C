e746723b0465152273eab5af2de1b4ba49e1fd77
aaronsanchez01 <aaronsanchez01@tamu.edu>
2021-12-03 15:14:29 -0600

    Copied stuff over from SimpleDisk.

--- P6/P6/blocking_disk.C
+++ P6/P6/blocking_disk.C
@@ -22,6 +22,7 @@
 #include "utils.H"
 #include "console.H"
 #include "blocking_disk.H"
+#include "machine.H"
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
@@ -29,6 +30,7 @@
 
 BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
   : SimpleDisk(_disk_id, _size) {
+    disk_id = _disk_id;
 }
 
 /*--------------------------------------------------------------------------*/
@@ -39,13 +41,56 @@ void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
 
   Console::puts("You must implement BlockingDisk::read\n");
   // -- REPLACE THIS with code that won't do busy waiting
-  SimpleDisk::read(_block_no, _buf);
+  issue_operation(READ, _block_no);
+
+  blocking_wait();
+
+  /* read data from port */
+  int i;
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++) {
+    tmpw = Machine::inportw(0x1F0);
+    _buf[i*2]   = (unsigned char)tmpw;
+    _buf[i*2+1] = (unsigned char)(tmpw >> 8);
+  }
 
 }
 
 
 void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
   Console::puts("You must implement BlockingDisk::write\n");
-  // -- REPLACE THIS with code that won't do busy waiting
-  SimpleDisk::write(_block_no, _buf);
+    issue_operation(WRITE, _block_no);
+
+  blocking_wait();
+
+  /* write data to port */
+  int i; 
+  unsigned short tmpw;
+  for (i = 0; i < 256; i++) {
+    tmpw = _buf[2*i] | (_buf[2*i+1] << 8);
+    Machine::outportw(0x1F0, tmpw);
+  }
+}
+
+void BlockingDisk::blocking_wait() {
+  Console::puts("We wait.");
+
+}
+
+void BlockingDisk::issue_operation(DISK_OPERATION _op, unsigned long _block_no) {
+
+  Machine::outportb(0x1F1, 0x00); /* send NULL to port 0x1F1         */
+  Machine::outportb(0x1F2, 0x01); /* send sector count to port 0X1F2 */
+  Machine::outportb(0x1F3, (unsigned char)_block_no);
+                         /* send low 8 bits of block number */
+  Machine::outportb(0x1F4, (unsigned char)(_block_no >> 8));
+                         /* send next 8 bits of block number */
+  Machine::outportb(0x1F5, (unsigned char)(_block_no >> 16));
+                         /* send next 8 bits of block number */
+  Machine::outportb(0x1F6, ((unsigned char)(_block_no >> 24)&0x0F) | 0xE0 | (disk_id << 4));
+                         /* send drive indicator, some bits, 
+                            highest 4 bits of block no */
+
+  Machine::outportb(0x1F7, (_op == READ) ? 0x20 : 0x30);
+
 }
