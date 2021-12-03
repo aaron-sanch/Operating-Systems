68eb4ddc3ca5abbe25d5330369605514ba606fe5
aaronsanchez01 <aaronsanchez01@tamu.edu>
2021-12-03 14:40:13 -0600

    Added P6

---
+++ P6/P6/blocking_disk.C
@@ -0,0 +1,51 @@
+/*
+     File        : blocking_disk.c
+
+     Author      : 
+     Modified    : 
+
+     Description : 
+
+*/
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "assert.H"
+#include "utils.H"
+#include "console.H"
+#include "blocking_disk.H"
+
+/*--------------------------------------------------------------------------*/
+/* CONSTRUCTOR */
+/*--------------------------------------------------------------------------*/
+
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
+  : SimpleDisk(_disk_id, _size) {
+}
+
+/*--------------------------------------------------------------------------*/
+/* SIMPLE_DISK FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
+
+  Console::puts("You must implement BlockingDisk::read\n");
+  // -- REPLACE THIS with code that won't do busy waiting
+  SimpleDisk::read(_block_no, _buf);
+
+}
+
+
+void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
+  Console::puts("You must implement BlockingDisk::write\n");
+  // -- REPLACE THIS with code that won't do busy waiting
+  SimpleDisk::write(_block_no, _buf);
+}
